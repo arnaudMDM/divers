96f0aa5ec5c0bf6b83d6f0359d78e905b6b0131b
arnaudMDM <arnaud.mdm@gmail.com>
2013-12-02 21:02:04 +0900

    deuxieme version encore mieuz

--- python/fn.py
+++ python/fn.py
@@ -1,29 +1,57 @@
-def f(n):
-    if n == 0:
-        return 0
 
-    nbDigit = 0
-    temp = n / 10
-    lastDigit = 0
-    while temp != 0:
-        nbDigit += 1
-        lastDigit = temp % 10
-        temp = temp / 10
-
-    if lastDigit == 0:
-        return 1
-
-    res = nbDigit * 10 ** (nbDigit - 1)
-    num = 10 ** nbDigit
-
-    if lastDigit > 1:
-        res = res * lastDigit + num + f(n - (num * lastDigit)) 
-    else:
-        res = res + n - num + 1 + f(n - num)
-
+def digit(n):
+    res = []
+    while True:
+        res.append(n % 10)
+        n = n / 10
+        if n == 0:
+            break
     return res
 
-def test(n):
-    for i in range(1, n):
-        if f(i) == i:
-            print i
+def printMax(listHaut, listBas, positif):
+    if listHaut != [] or listBas != []:
+        if positif:
+            print "MAX: ", max(listHaut)
+        else:
+            print "MIN: ", min(listBas)
+
+def f(n):
+    listBas = []
+    listHaut = []
+    res = 0
+    plus = False
+    positif = False
+    maxValue = 0
+    for i in range(1, n + 1):
+        listDigit = digit(i)
+        for j in listDigit:
+            if j == 1:
+                res += 1
+        if i == res:
+            print "equal: ", i
+            printMax(listHaut,listBas,positif)
+            listHaut=[]
+            listBas=[]
+        elif plus:
+            if (res - i > 0 and not positif) or (res - i < 0 and positif):
+                printMax(listHaut,listBas,positif)
+                positif = not positif
+                listHaut = []
+                listBas = []
+
+            if res - i < maxValue:
+                # print "haut: ", i-1
+                listHaut.append(i-1)
+                plus = False
+        elif not plus:
+            if (res - i > 0 and not positif) or (res - i < 0 and positif):
+                printMax(listHaut,listBas,positif)
+                positif = not positif
+                listHaut = []
+                listBas = []
+
+            if res - i > maxValue:
+                # print "bas: ", i-1
+                listBas.append(i-1)
+                plus = True
+        maxValue = res - i
