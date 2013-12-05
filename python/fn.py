6e46ad9f61abd58aff8711829bf60d49253610e8
arnaudMDM <arnaud.mdm@gmail.com>
2013-12-05 15:10:15 +0900

    fnFinal implmentation enough for secind last

--- python/fn.py
+++ python/fn.py
@@ -8,12 +8,17 @@ def digit(n):
             break
     return res
 
-def f(n):
-    res=0
-    for i in range(1, n + 1):
+def f(n,value, n2):
+    res=value
+    for i in range(n, n2,-1):
         listDigit = digit(i)
         for j in listDigit:
             if j == 1:
-                res += 1
-        if i == res:
-            print i
+                res -= 1
+        if i-1 == res:
+            print i-1
+
+# min=199981 max=200001 moy=10 (6 digits) maxHigh=1 coef premiere courbe=0.5000450040503646
+# min=1599981 max=2600001 moy=500010 (7 digits) maxHigh=200001 coef premiere courbe=0.6000054000486005
+# min=13199998 max=35200001 moy=11000001.5 (8 digits) maxHigh=4200001 and 1 coef premiere courbe=0.6000004500003375
+# 1111111110 peut etre le dernier
--- python/fnFinal.py
+++ python/fnFinal.py
@@ -72,15 +72,38 @@ def findFromLastSimple(sign,nb,nbSoustract,n):
         elif nb / nbSoustract == 1: 
             return nb2, nbFound
 
+def checkNoMore(digit, nbMin, nbMax):
+    global DIGIT_LIMIT
+    digitIndex = DIGIT_LIMIT
+    maxDigit = 10 ** digit
+    nb = nbMin
+    nbAdd = 10 ** (digitIndex - 1)
+    while True:
+        if digitIndex >= digit:
+            return True
+        nb = nbMin / 10 / nbAdd * nbAdd * 10 + nbAdd
+        for i in range(digitIndex - 1):
+            nb = nb + 9 * 10 ** i
+        if nb < nbMin:
+            nb = nb + (nbAdd * 10)
+            if nb >= maxDigit:
+                return True
+        if nb >= nbMax:
+            return True
+        if f(nb) - nb >= 0:
+            print nb, digitIndex
+            return False
+        nbAdd *= 10
+        digitIndex += 1
+
 def getLastInStep(digit, n):
     nb = 0
-    nb2 = 0
     nbFound = 0
     for i in range(digit):
         nb += 9 * 10 ** i
-    nbSoustract = 10 ** (digit - 1)
-    global DIGIT_LIMIT
-    digitIndex = DIGIT_LIMIT
+    maxDigit = 10 ** (digit - 1)
+    nbSoustract = maxDigit
+    digitIndex = 1
     plus = False
 
     if f(nb) - nb == 0:
@@ -98,7 +121,11 @@ def getLastInStep(digit, n):
         if not plus:
             nb2,nbFound2 = findFromLastSimple(False, nb, nbSoustract, n - nbFound)
             nbFound += nbFound2
-            if nbFound == n:
+            # print nb
+            if not checkNoMore(digit, nb2 + nbFound2, nb):
+                raise Exception("algorithm not completed")
+
+            if nbFound == n or nbFound == 0:
                 return nb2, nbFound
             else:
                 nb = nb2 - 1
@@ -112,14 +139,14 @@ def getLastInStep(digit, n):
             for i in range(digitIndex - 1):
                 nb1 = nb1 + 9 * 10 ** i
                 nb2 = nb2 + 9 * 10 ** i
-            if f(nb1) - nb1 < 0:
-                nb2,nbFound2 = findFromLastSimple(True, nb2, temp, n - nbFound)
+            if f(nb1) - nb1 <= 0:
+                nb3,nbFound2 = findFromLastSimple(True, nb2, temp, n - nbFound)
                 nbFound += nbFound2
-                if nbFound == n or nb1 / temp == 0:
-                    return nb2, nbFound
+                if nbFound == n or nb2 / maxDigit == 1:
+                    return nb3, nbFound
                 else:
-                    nb = nb2 - 1
-                    digitIndex = DIGIT_LIMIT
+                    nb = nb3 - 1
+                    digitIndex = 1
                     plus = False
             else:
                 digitIndex += 1
@@ -131,5 +158,7 @@ def getLast(n):
         nb,nbFound2 = getLastInStep(i,n - nbFound)
         nbFound += nbFound2
         if nbFound == n:
-            print nb
-            break
+            return nb
+
+def test():
+    print getLastInStep(9, 1)
