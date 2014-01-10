e1f6796b7013c3bda371fef4dc6123ba0aac443f
unknown <arnaud.mdm@gmail.com>
2014-01-10 10:32:04 +0900

    big_sort works

--- python/big_sort.py
+++ python/big_sort.py
@@ -1,15 +1,50 @@
 import string
 import re
+import os
 
-f=open("test.txt", "r")
-a=f.read(500000000)
-b=[a[i:i+18] for i in range(0, len(a), 18)]
-b.sort()
-g=open("temp","w")
-g.write(bytearray("".join(b)))
-g.close()
+nbFile = 100
+f = open("test2.txt", "rb")
+for i in range(nbFile):
+    a = f.read(99999990)
+    b = [a[j:j+18] for j in range(0, len(a), 18)]
+    b.sort()
+    # print '\n'.join(':'.join(x.encode('hex') for x in r) for r in b)
+    g = open("temp0"+str(i),"wb")
+    g.write(bytearray("".join(b)))
+    g.close()
 f.close()
-# for i in b:
-#     print ':'.join(j.encode('hex') for j in i)
 
-# print ':'.join(x.encode('hex') for x in a)
+i = 0
+while nbFile > 1:
+    for j in range(0, nbFile / 2 * 2, 2):
+        f = open("temp" + str(i) + str(j), "rb")
+        g = open("temp" + str(i) + str(j + 1),"rb")
+        h = open("temp" + str(i + 1) + str(j / 2), "wb")
+        a = f.read(18)
+        b = g.read(18)
+        while True:
+            if not a and not b:
+                break
+            elif not a:
+                h.write(bytearray(b))
+                h.write(bytearray(g.read()))
+                break
+            elif not b:
+                h.write(bytearray(a))
+                h.write(bytearray(f.read()))
+                break
+            elif a < b:
+                h.write(bytearray(a))
+                a = f.read(18)
+            else:
+                h.write(bytearray(b))
+                b = g.read(18)
+        f.close()
+        g.close()
+        h.close()
+    if nbFile % 2 > 0:
+        os.rename("temp" + str(i) + str(nbFile - 1), "temp" + str(i + 1) + str(nbFile / 2))
+        nbFile = nbFile / 2 + 1
+    else:
+        nbFile = nbFile / 2
+    i += 1
--- python/file.py
+++ python/file.py
@@ -4,12 +4,12 @@ import os
 random.seed()
 
 #os.remove("C:\\users\\arnaud\\test.txt")
-fo = open("C:\\users\\arnaud\\test.txt", "r+b")
-fo.seek(16,2)
+fo = open("C:\\users\\arnaud\\test.txt", "wb") # r+b
+# fo.seek(16,2)
 byte = bytearray(18)
 byte[16] = 13
 byte[17] = 10
-for i in range (257254902):
+for i in range (3): # 257254902
     for j in range(16):
         byte[j] = random.randrange(16)
     fo.write(byte)
